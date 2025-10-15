df9dc743a72c43a79b6830f3d387463fd4cb534f
Ana Caroline Vasconcellos <anawold2008@gmail.com>
2025-10-15 15:06:08 +0000

    test

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -44,16 +44,16 @@ urlpatterns = [
     path(
         'reset/<uidb64>/<token>/', 
         auth_views.PasswordResetConfirmView.as_view(
-            template_name='registration/password_reset_complete.html', 
+            template_name='registration/password_reset_confirm.html', 
             success_url=reverse_lazy('password_reset_complete') 
         ), 
-        name='password_reset_complete'
+        name='password_reset_confirm'
     ),
     path(
         'reset/done/', 
         auth_views.PasswordResetCompleteView.as_view(
-            template_name='registration/password_reset_confirm.html' 
+            template_name='registration/password_reset_complete.html' 
         ), 
-        name='password_reset_confirm'
+        name='password_reset_complete'
     ),
 ]
